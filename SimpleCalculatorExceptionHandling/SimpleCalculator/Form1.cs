3152341b30b43f11d3b933fd9dc2c3e7babaaf55
kdooley727 <104645526+kdooley727@users.noreply.github.com>
2022-05-19 08:11:29 -0700

    Extra Exercise 7-1

---
+++ SimpleCalculatorExceptionHandling/SimpleCalculator/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            decimal operand1 = Convert.ToDecimal(txtOperand1.Text);
+            string operator1 = txtOperator.Text;
+            decimal operand2 = Convert.ToDecimal(txtOperand2.Text);
+            decimal result = Calculate(operand1, operator1, operand2);
+
+            result = Math.Round(result, 4);
+            this.txtResult.Text = result.ToString();
+            txtOperand1.Focus();
+        }
+
+        private decimal Calculate(decimal operand1, string operator1,
+            decimal operand2)
+        {
+            decimal result = 0;
+            if (operator1 == "+")
+                result = operand1 + operand2;
+            else if (operator1 == "-")
+                result = operand1 - operand2;
+            else if (operator1 == "*")
+                result = operand1 * operand2;
+            else if (operator1 == "/")
+                result = operand1 / operand2;
+            return result;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ClearResult(object sender, EventArgs e)
+        {
+            this.txtResult.Text = "";
+        }
+    }
+}
